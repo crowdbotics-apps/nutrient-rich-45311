ae371e0d3e53ca77c30774d37920337df31a78a2
Crowdbotics <team@crowdbotics.com>
2024-01-07 23:36:54 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "nutrient-rich-45311",
+  url: "https://nutrient-rich-45311.botics.co",
+  api: "https://nutrient-rich-45311.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
